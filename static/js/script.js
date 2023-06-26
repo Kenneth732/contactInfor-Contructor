5bad7f176597a1f0c519fae338c69ecffaf5ca89
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-26 13:26:01 +0300

    dislayContact function for rendering data in contacts array

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,23 @@ function Contact(firstName, lastName, phoneNumber, emailAddress) {
 
 let contacts = [];
 
+function displayContacts() {
+    const contactContainer = document.querySelector('#contacts')
+    contactContainer.innerHTML = '';
+
+    if(contacts.length === 0) {
+        contactContainer.innerHTML = '<p>No contact found...</p>'
+    }else{
+        contacts.forEach((contact, index) => {
+            const contactElement = document.createElement('div')
+            contactElement.classList.add('contact')
+            contactElement.innerHTML = "<h3>" + contact.firstName + "</h3>" +
+            "<h3>" + contact.lastName + "</h3>" +
+            "<p>" + contact.phoneNumber +"</p>" +
+            "<p>" + contact.emailAddress +"</p>" +
+            "<button class='editContact("+ index +")'>Edit</button>" +
+            "<button class='deleteContact("+ index +")'>Delete</button>" ;
+            contactContainer.appendChild(contactElement);
+        })
+    }
+}
